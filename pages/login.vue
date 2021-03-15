a02dca3b852d498bad4b0d38ba6d74ff4c2658f7
Nicolas Martins <nicolas.martins@hotmail.fr>
2021-03-15 13:36:10 +0100

    Static login page done

--- pages/login.vue
+++ pages/login.vue
@@ -1,21 +1,16 @@
 <template>
   <main class="container">
-    <h1 class="form-header">Formulaire d'inscription</h1>
+    <h1 class="form-header">Formulaire de connexion</h1>
     <form class="form-body">
       <input id="email" type="email" placeholder="Email" />
       <input id="password" type="password" placeholder="Mot de passe" />
-      <input
-        id="confirmPassword"
-        type="password"
-        placeholder="Confirmation mot de passe"
-      />
       <button id="submitButton" type="submit" class="btn-default">
-        Inscription
+        Connexion
       </button>
     </form>
     <aside class="form-aside">
-      <h2>Déjà client ?</h2>
-      <nuxt-link to="/login" class="btn-default">Connexion</nuxt-link>
+      <h2>Pas encore client ?</h2>
+      <nuxt-link to="/signin" class="btn-default">Inscription</nuxt-link>
     </aside>
   </main>
 </template>
