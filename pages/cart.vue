a01e79eb9b5461847ac5a68bdb354bbe628ee9c9
Nicolas Martins <nicolas.martins@hotmail.fr>
2021-03-19 09:31:55 +0100

    created cart page. Static ok, need to do the interaction between add to cart button on the product page and display on the cart page

---
+++ pages/cart.vue
@@ -0,0 +1,43 @@
+<template>
+  <main class="container">
+    <h2 class="text-center">Récapitulatif de votre panier</h2>
+    <table>
+      <thead class="color-primary">
+        <tr>
+          <th>Produit</th>
+          <th>Prix</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Xiamio Redmi Note 8T</td>
+          <td>500 €</td>
+        </tr>
+        <tr class="color-primary">
+          <td>Prix total</td>
+          <td>500 €</td>
+        </tr>
+      </tbody>
+    </table>
+    <button class="btn-default border-color">Valider mon panier</button>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+h2 {
+  margin: 1.2rem 0;
+}
+tr.color-primary {
+  font-size: 1.1rem;
+  background-color: #fff;
+}
+button {
+  margin: 0 auto;
+  display: block;
+  width: 230px;
+}
+</style>
